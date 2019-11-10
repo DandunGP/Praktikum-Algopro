6ed869cce4657048a954f8469d442f6ad7fc4767
Dandun Gigih Prakoso L200190172 <57583849+L200190172@users.noreply.github.com>
2019-11-10 02:02:20 -0800

    Add files via upload

---
+++ Praktikum 6/Kegiatan1.py
@@ -0,0 +1,22 @@
+## Program Identitas Data Diri. Dibuat oleh Dandun L200190172
+Nama = 'Dandun Gigih Prakoso'
+Tempat_Tanggal_Lahir = 'Klaten 25 Juni 2001'
+Jenis_Kelamin = 'Laki-laki'
+Alamat = 'Sritinon, Delanggu, Delanggu, Klaten, Jawa Tengah'
+No_HP = '082313822787'
+Agama = 'Islam'
+Status_Perkawinan = 'Belum Menikah'
+Pekerjaan = 'Pelajar/Mahasiswa'
+Kewarganegaraan = 'Indonesia'
+Golongan_Darah = 'AB'
+
+print(Nama)
+print(Tempat_Tanggal_Lahir)
+print(Jenis_Kelamin)
+print(Alamat)
+print(No_HP)
+print(Agama)
+print(Status_Perkawinan)
+print(Pekerjaan)
+print(Kewarganegaraan)
+print(Golongan_Darah)
